8d40168daa4a63412370f10f43fd6384db4a5b44
Cheb70392 <vitaly_chebotarev@mail.ru>
2015-09-13 13:33:25 +0300

    'JAVA-file'

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Виталий on 13.09.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Vitaly!");
+    }
+}
